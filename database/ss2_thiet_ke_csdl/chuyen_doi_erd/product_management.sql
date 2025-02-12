0ed192eb1d19f2a73df5d88f80123ef3ef54bede
lqc02ht <cuong02ht@gmail.com>
2025-02-12 18:15:55 +0700

    nothing

--- database/ss2_thiet_ke_csdl/chuyen_doi_erd/product_management.sql
+++ database/ss2_thiet_ke_csdl/chuyen_doi_erd/product_management.sql
@@ -3,32 +3,32 @@ CREATE DATABASE product_management;
 USE product_management;
 
 CREATE TABLE provider (
-	id INT PRIMARY KEY,
-	`name` VARCHAR(100),
+    id INT PRIMARY KEY,
+    `name` VARCHAR(100),
     addresss VARCHAR(50),
     phone VARCHAR(10)
 );
 
 CREATE TABLE phone (
-	id INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     provider_id INT,
     FOREIGN KEY (provider_id) REFERENCES provider(id)
 );
 
 CREATE TABLE orders (
-	id INT PRIMARY KEY,
-	`date` DATETIME,
+    id INT PRIMARY KEY,
+    `date` DATETIME,
     orders_id INT,
     FOREIGN KEY (orders_id) REFERENCES provider(id)
 );
 
 CREATE TABLE products (
-	id INT PRIMARY KEY,
-	name VARCHAR(100)
+    id INT PRIMARY KEY,
+    name VARCHAR(100)
 );
 
 CREATE TABLE orders_detail (
-	order_id INT,
+    order_id INT,
     product_id INT,
     PRIMARY KEY (order_id, product_id),
     FOREIGN KEY (order_id) REFERENCES orders(id),
@@ -36,19 +36,19 @@ CREATE TABLE orders_detail (
 );
 
 CREATE TABLE delivery (
-	id INT PRIMARY KEY,
-	`date` DATETIME
+    id INT PRIMARY KEY,
+    `date` DATETIME
 );
 
 CREATE TABLE received (
-	id INT PRIMARY KEY,
-	`date` DATETIME
+    id INT PRIMARY KEY,
+    `date` DATETIME
 );
 
 CREATE TABLE delivery_detail (
-	delivery_id INT,
+    delivery_id INT,
     product_id INT,
-	price DOUBLE,
+    price DOUBLE,
     `number` INT,
     PRIMARY KEY (delivery_id, product_id),
     FOREIGN KEY (delivery_id) REFERENCES delivery(id),
@@ -56,9 +56,9 @@ CREATE TABLE delivery_detail (
 );
 
 CREATE TABLE received_detail (
-	received_id INT,
+    received_id INT,
     product_id INT,
-	price DOUBLE,
+    price DOUBLE,
     `number` INT,
     PRIMARY KEY (received_id, product_id),
     FOREIGN KEY (received_id) REFERENCES received(id),
--- database/ss2_thiet_ke_csdl/xay_dung_csdl/products_management.sql
+++ database/ss2_thiet_ke_csdl/xay_dung_csdl/products_management.sql
@@ -3,27 +3,27 @@ CREATE DATABASE products_management;
 USE products_management;
 
 CREATE TABLE customer (
-	id INT PRIMARY KEY,
-	`name` VARCHAR(100),
+    id INT PRIMARY KEY,
+    `name` VARCHAR(100),
     age INT
 );
 
 CREATE TABLE `order` (
-	id INT PRIMARY KEY,
-	`date` DATETIME,
+    id INT PRIMARY KEY,
+    `date` DATETIME,
     price DOUBLE,
     customer_id INT,
     FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
 
 CREATE TABLE product (
-	id INT PRIMARY KEY,
-	name VARCHAR(100),
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
     price DOUBLE
 );
 
 CREATE TABLE order_detail (
-	order_id INT,
+    order_id INT,
     product_id INT,
     quantity INT,
     PRIMARY KEY (order_id, product_id),
